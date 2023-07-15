580230bf6982362f3696f330d7c670296c51dd10
Fahim <fahimmontasir74@gmail.com>
2023-07-15 21:48:24 +0600

    hasvdh

--- login.php
+++ login.php
@@ -1,3 +1,58 @@
+<?php
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get form data
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Perform database operations (Replace with your own database connection code)
+    $connection = mysqli_connect('localhost', 'username', 'password', 'database_name');
+    
+    // Check if the connection was successful
+    if (!$connection) {
+        die('Database connection failed: ' . mysqli_connect_error());
+    }
+
+    // Sanitize the user input
+    $username = mysqli_real_escape_string($connection, $username);
+
+    // Retrieve the user's password hash from the database
+    $query = "SELECT password FROM users WHERE username = '$username'";
+    $result = mysqli_query($connection, $query);
+
+    // Check if the query executed successfully
+    if ($result) {
+        // Check if the user exists in the database
+        if (mysqli_num_rows($result) === 1) {
+            $row = mysqli_fetch_assoc($result);
+            $hashedPassword = $row['password'];
+
+            // Verify the password
+            if (password_verify($password, $hashedPassword)) {
+                // Password is correct, set session variables and redirect to the profile page
+                session_start();
+                $_SESSION['username'] = $username;
+                header("Location: profile.php");
+                exit;
+            } else {
+                // Password is incorrect
+                $errorMessage = "Invalid username or password";
+            }
+        } else {
+            // User does not exist
+            $errorMessage = "Invalid username or password";
+        }
+    } else {
+        // Handle any errors during the database operation
+        $errorMessage = mysqli_error($connection);
+        // You can display or handle the error as per your project's requirements
+    }
+
+    // Close the database connection
+    mysqli_close($connection);
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +60,10 @@
 </head>
 <body>
     <h1>Login</h1>
-    <form action="login-process.php" method="POST">
+    <?php if (isset($errorMessage)): ?>
+        <p><?php echo $errorMessage; ?></p>
+    <?php endif; ?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
         <label for="username">Username:</label>
         <input type="text" name="username" required><br>
 
